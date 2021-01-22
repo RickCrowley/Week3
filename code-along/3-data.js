d9c992b01f0b2dea93288fc5218236a5e37d9e53
RickCrowley <rick.crowley@kellogg.northwestern.edu>
2021-01-21 19:51:19 -0600

    lesson 3

--- code-along/3-data.js
+++ code-along/3-data.js
@@ -1,20 +1,36 @@
 // 👇 YOUR CODE BEGINS HERE. DON'T CHANGE ANY OTHER CODE. 👇
 
 // 🔥🔥🔥 YOUR CODE GOES HERE 🔥🔥🔥
+let profile = {
+  name: 'Brian',
+  location: { city: 'Chicago', state: 'Illinois' },
+  timeline: [
+    { icon: '🧠', when: '2020-10-26 17:00:00', what: 'Teaching MBA students how to code in JavaScript!' },
+    { icon: '🌮', when: '2020-10-26 12:00:00', what: 'Eating tacos' },
+    { icon: '🦄', when: '2020-10-26 08:00:00', what: 'Riding a unicorn' }
+  ]
+}
+
 // let profile = {
-//   name: 'Brian',
-//   location: { city: 'Chicago', state: 'Illinois' },
-//   timeline: [
-//     { icon: '🧠', when: '2020-10-26 17:00:00', what: 'Teaching MBA students how to code in JavaScript!' },
-//     { icon: '🌮', when: '2020-10-26 12:00:00', what: 'Eating tacos' },
-//     { icon: '🦄', when: '2020-10-26 08:00:00', what: 'Riding a unicorn' }
-//   ]
+//   name: 'Rick',
+//   location: 'St. John',
+//   status: 'Got it!'
 // }
-//  👆 YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE 👆
+// // console.log(profile)
+
+// let shoppingList =['eggs', 'milk', 'stick of butter']
+// shoppingList.push('lottery ticket')
+// console.log(shoppingList)
+
+// let lotteryTicket = [4, 8, 15, 16, 23, 43]
+// console.log(lotteryTicket[0])
+
+
+//👆 YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE 👆
 
 // template literals support HTML too!
 output = `
-  <div class="text-4xl">Sweet</div>
+  <div class="text-4xl">${profile.timeline[0].icon}</div>
 `
 /*
   this code does the work of writing the output to screen...
